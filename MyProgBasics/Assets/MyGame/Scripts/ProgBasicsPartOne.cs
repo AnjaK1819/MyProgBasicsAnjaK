d7800af8e526db032cead2cbdda42c5cf2efbd73
AnjaK1819 <anja.kriechhammer@gmail.com>
2019-03-13 17:42:58 +0100

    hallo2

--- MyProgBasics/Assets/MyGame/Scripts/ProgBasicsPartOne.cs
+++ MyProgBasics/Assets/MyGame/Scripts/ProgBasicsPartOne.cs
@@ -3,14 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ProgBasicsPartOne : MonoBehaviour {
+    
+    private int index = 0;
+    private int step = 5;
+    private int myStep = 3;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		
 	}
+    
+    
+    void Update ()
+    {
+        CountUp(3);
+        Text("myStringText");
+        MultNumbers(3.3f, 1.0f);
+        MixTypes("firstTry", 4, 3.9f);
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
+	private void CountUp()
+    {
+		index = index + step;
+    //  index += step;
+        Debug.Log(index);
 	}
+    
+    private void CountUp(int myStep)
+    {
+		index = index + myStep;
+        Debug.Log(index);
+	}
+ 
+    private void Text(string text)
+    {
+        Debug.Log(text);
+    }
+    
+    private void MultNumbers(float first, float second)
+    {
+        float tmp = first * second;
+        Debug.Log("MultNumbers: " + tmp);
+    }
+    
+    private void MixTypes(string test, int index, float end)
+    {
+        Debug.Log(test + " , " + index + " , " + end);
+    }
 }
